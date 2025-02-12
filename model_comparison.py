66b0e1655497a77e86eb7fbe08a978b6bfcb7009
Pengyao Zhao <Pengyao.Zhao@smu.ca>
2025-02-12 10:07:18 -0400

    try more models

--- model_comparison.py
+++ model_comparison.py
@@ -45,31 +45,38 @@ def generate_embedding(file_name='MCDA5511-classmates - 2025.csv', model='all-Mi
 def compute_similarity_single_to_many(embedding, person_name='Max Zhao'):
 
     query_embedding = embedding[person_name].reshape(1, -1)
-    candidate_embeddings = np.array([v for k, v in embedding.items() if k != person_name])
-    
+    candidate_names = [k for k in embedding.keys() if k != person_name]
+    candidate_embeddings = np.array([embedding[k] for k in candidate_names])
+
     similarities = cosine_similarity(query_embedding, candidate_embeddings)
     return similarities
 
-def compute_spearman_correlation(embeddings1, embeddings2, name='Max Zhao'):
+def compute_spearman_correlation(embeddings1, embeddings2, name='Max Zhao', topN=-1):
+
+    topN = len(embeddings1.keys()) if topN == -1 else topN
 
     similarities1 = compute_similarity_single_to_many(embeddings1, name)
     similarities2 = compute_similarity_single_to_many(embeddings2, name)
-    ranking1 = np.argsort(similarities1[0]) + 1
-    ranking2 = np.argsort(similarities2[0]) + 1
+    ranking1 = np.argsort(-1 * similarities1[0])[: topN] + 1
+    ranking2 = np.argsort(-1 * similarities2[0])[: topN] + 1
+    print(ranking1)
+    print(ranking2)
 
     rho, _ = spearmanr(ranking1, ranking2)
-    print(f"Spearman’s 𝜌: {rho}")
     return rho
 
 if __name__ == '__main__':
 
     person_embeddings1 = generate_embedding(model='all-MiniLM-L6-v2')
     person_embeddings2 = generate_embedding(model='all-MiniLM-L12-v2')
+    person_embeddings3 = generate_embedding(model='all-mpnet-base-v2')
 
-    names = ['Max Zhao', 'Yaxuan Zhang', 'SicongFu']
+    names = ['Max Zhao'] #, 'Yaxuan Zhang', 'SicongFu']
     for name in names:
 
-        rho = compute_spearman_correlation(person_embeddings1, person_embeddings2, name)
+        # rho = compute_spearman_correlation(person_embeddings1, person_embeddings2, name)
+        rho = compute_spearman_correlation(person_embeddings1, person_embeddings3, name)
+        # rho = compute_spearman_correlation(person_embeddings2, person_embeddings3, name)
         print(f'correlation={rho:.4f}')
 
 
