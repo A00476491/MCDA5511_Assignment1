e7ebbfefd6ee28106a3b9d808f8b1323e3b6d593
Pengyao Zhao <Pengyao.Zhao@smu.ca>
2025-02-12 14:06:10 -0400

    test

--- optimize_UMAP.py
+++ optimize_UMAP.py
@@ -20,6 +20,7 @@ from scipy.stats import spearmanr
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from itertools import product
+import copy
 
 project_path = './'
 
@@ -54,10 +55,13 @@ def compute_spearman_correlation(embeddings1, embeddings2, name='Max Zhao'):
 
     similarities1 = compute_similarity_single_to_many(embeddings1, name)
     similarities2 = compute_similarity_single_to_many(embeddings2, name)
+    print(similarities1)
     ranking1 = np.argsort(-1 * similarities1[0]) + 1
     ranking2 = np.argsort(-1 * similarities2[0]) + 1
 
     rho, _ = spearmanr(ranking1, ranking2)
+    print(ranking1)
+    print(ranking2)
     print(f"Spearman’s 𝜌: {rho}")
     return rho
 
@@ -79,12 +83,15 @@ def do_UMAP(embeddings, parameters, vis_name=None, seed=42):
     reduced_data = reducer.fit_transform(scaled_data)
     reduced_embedding = {key: reduced_data[i] for i, (key, value) in enumerate(person_embeddings.items())}
 
+    # print('############', len(reduced_data))
+
     if vis_name is not None:
         x = [row[0] for row in reduced_data]
         y = [row[1] for row in reduced_data]
         label = list(person_embeddings.keys())
 
         # Plotting and annotating data points
+        plt.figure(figsize=(6, 6)) 
         plt.scatter(x,y)
         for i, name in enumerate(label):
             plt.annotate(name, (x[i], y[i]), fontsize="8")
@@ -100,10 +107,10 @@ if __name__ == '__main__':
     person_embeddings = generate_embedding(model='all-MiniLM-L6-v2')
 
     # search space
-    n_neighbors_list = [5, 8, 10, 13, 15, 20]
-    min_dist_list = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    n_neighbors_list = [5] # [5, 8, 10, 13, 15, 20]
+    min_dist_list = [0.9] # [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
-    # do search
+    # best parameter search
     best_score = -np.inf
     best_params = {}
     for n_neighbors, min_dist in product(n_neighbors_list, min_dist_list):
@@ -120,8 +127,9 @@ if __name__ == '__main__':
 
     print(f'Best params: {best_params}, Best score: {best_score:.4f}')
 
-    reduced_data = do_UMAP(person_embeddings, best_params, seed=47)
-    score = compute_spearman_average_correlation(person_embeddings, reduced_data)
+    reduced_data = do_UMAP(person_embeddings, best_params, seed=42, vis_name='vis1.png')
+    reduced_data2 = do_UMAP(person_embeddings, best_params, seed=43, vis_name='vis2.png')
+    score = compute_spearman_average_correlation(reduced_data2, reduced_data)
     print(f'correlation={score:.4f}')
 
 
