0183d1300483c8883bf98db0c447352f66275829
Pengyao Zhao <Pengyao.Zhao@smu.ca>
2025-02-14 20:58:03 -0400

    refine code style

--- optimize_UMAP.py
+++ optimize_UMAP.py
@@ -215,9 +215,9 @@ if __name__ == '__main__':
     
     # Step4: Perform grid search to optimize UMAP parameters
     search_space = {
-        'n_neighbors': [10], # [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        'min_dist': [0.25], # [0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 0.7, 0.9],
-        'spread': [8], # [0.1, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], # [10]
+        'min_dist': [0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 0.7, 0.9], # [0.25]
+        'spread': [0.1, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9], # [8]
     }
     best_params = grid_search(person_embeddings, search_space)
      
